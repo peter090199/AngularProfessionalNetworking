904edcee80c716b56a9d115bb60325930f525a83
Peds Yorpo <pedroyorpo17@gmail.com>
2024-11-19 00:08:58 +0800

    commit

--- src/app/services/Global/notifications.service.ts
+++ src/app/services/Global/notifications.service.ts
@@ -67,21 +67,8 @@ export class NotificationsService {
   
 toastPopUp(msg:string){
   return Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Deleted!",
-        text: "Your file has been deleted.",
-        icon: "success"
-      });
-    }
+    text: msg,
+    icon: "success"
   });
 }
 
